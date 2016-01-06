3d12667e0cbbc111c4f40769a84a34d943062f78
huangyoucheng <huangyoucheng@baidu.com>
2016-01-06 21:27:05 +0800

    Initial Commit

---
+++ ScanBarcodeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScanBarcodeTest
+//
+//  Created by baidu on 16/1/6.
+//  Copyright © 2016年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScanBarcodeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScanBarcodeTest
+//
+//  Created by baidu on 16/1/6.
+//  Copyright © 2016年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
